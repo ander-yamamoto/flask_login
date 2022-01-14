6ea4c34a4fb4a34ff086971dc8730a7ac584d2bf
Anderson Yamamoto <ander182@gmail.com>
2022-01-13 22:50:54 -0300

    add node table to database model

--- project/models.py
+++ project/models.py
@@ -6,3 +6,15 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+
+
+class Node(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False) #INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+    name = db.Column(db.String(100), unique=True, nullable=False) #TEXT NOT NULL UNIQUE,
+    topic = db.Column(db.String(100), nullable=False) #TEXT NOT NULL,
+    item_id = db.Column(db.String(100), nullable=False) #TEXT NOT NULL,
+    ip = db.Column(db.String(16), nullable=False) #VARCHAR(16) NOT NULL,
+    status = db.Column(db.Integer, nullable=False) #INTEGER NOT NULL,
+    last_update = db.Column(db.DateTime, nullable=False) #timestamp NOT NULL
+
